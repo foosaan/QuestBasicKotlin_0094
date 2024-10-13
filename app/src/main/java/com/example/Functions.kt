72978b799adc495982e1b1c563210c0063ee8a11
fauzan alfikri <112147642+foosaan@users.noreply.github.com>
2024-10-13 20:27:38 +0700

    menambahkan fungsi dengan parameter

--- app/src/main/java/com/example/Functions.kt
+++ app/src/main/java/com/example/Functions.kt
@@ -4,3 +4,8 @@ fun withoutParameter() {
     println("= without Parameter ==")
     println("Hello, World!")
 }
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!") // Menampilkan pesan dengan nama yang diberikan
+}
