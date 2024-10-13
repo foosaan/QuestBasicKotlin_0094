44832451ed82d7c8603fd3fdf97ca09f661feccb
fauzan alfikri <112147642+foosaan@users.noreply.github.com>
2024-10-13 21:12:17 +0700

    Fungsi untuk menunjukkan fitur null safety

---
+++ app/src/main/java/com/example/NullSefety.kt
@@ -0,0 +1,26 @@
+package com.example
+
+fun nullSafety(){
+    var neverNull: String = "This can't be null"
+
+    var nullable: String?  = "you can keep a null here"
+    nullable = null
+
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+
+    }else{
+        println("inferredNonNull is not null")
+
+        println(neverNull.length)
+        println(nullable?.length)
+    }
+    fun strLength(notNull: String): Int{
+        return notNull.length
+    }
+    println(strLength(neverNull))
+}
+
+fun main (){
+    nullSafety()
+}
