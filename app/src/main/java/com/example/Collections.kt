cb1bf2da5679aab41affb3bf9b6a86a34cb53f44
fauzan alfikri <112147642+foosaan@users.noreply.github.com>
2024-10-13 13:24:53 +0700

    membuat function contohMap yang berisi val readOnly untukk membuat list yang bersifat read only, val shape untuk membuat mutable list, val shapeslocked untuk mengunci list menjadi readonly

--- app/src/main/java/com/example/Collections.kt
+++ app/src/main/java/com/example/Collections.kt
@@ -50,3 +50,40 @@ fun ContohSet() {
 
 }
 
+
+fun ContohMap() {
+
+    println()
+
+    println("== Map ==")
+
+// Map Read-Only
+
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+// Map Mutable
+
+// Menambahkan data ke dalam Map Mutable
+
+    shape["Rectangle"] = 4
+    println(shape)
+
+// Menghapus data dari Map Mutable
+
+    shape.remove("Circle")
+    println(shape)
+
+// Mengubah data di dalam Map Mutable
+
+    shape["Square"] = 5
+    println(shape)
+
+// Map Read-Only
+
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
+}
