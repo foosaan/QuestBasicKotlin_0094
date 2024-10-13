665f108f0609f484b69c28819ecbb678660bea2d
fauzan alfikri <112147642+foosaan@users.noreply.github.com>
2024-10-13 20:16:11 +0700

    membuat fungsi untuk menunjukkan penggunaan ekpressi kondisional, val number untuk mengecek apakan number positif atau negatif, val day untuk menentukan nama hari berdasarkan nilai day

---
+++ app/src/main/java/com/example/ConditionalExpression.kt
@@ -0,0 +1,34 @@
+package com.example
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+
+    } else {
+        println("Negative Number")
+
+    }
+
+    val day = 1
+    when (day){
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Fay")
+    }
+    val value ="10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException){
+        println("Invalid Number")
+    }
+
+}
