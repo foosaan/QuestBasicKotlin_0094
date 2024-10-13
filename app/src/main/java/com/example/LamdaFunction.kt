5e5673fcf1e8982d97e637b7c192ee504c5e3a4d
fauzan alfikri <112147642+foosaan@users.noreply.github.com>
2024-10-13 21:07:27 +0700

    menambahkan fungsi untuk mengubah string menjadi huruf kapital

---
+++ app/src/main/java/com/example/LamdaFunction.kt
@@ -0,0 +1,11 @@
+package com.example
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
+
+fun main(){
+    uppercaseString("Hallo")
+    println({ string: String -> string.uppercase()}("Hallo"))
+
+}
